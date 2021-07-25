4ec1eda91fb36c7bee36237ea0307000c770a15e
tenefra <stenefra@gmail.com>
2021-07-24 20:21:44 -0500

    axios request for front page posts added

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,7 +1,13 @@
-import React, { useEffect } from "react"
+import React from "react"
 
-function List() {
-  return <></>
+function List(props) {
+  return (
+    <li>
+      <h5>{props.points}</h5>
+      <h3>{props.title}</h3>
+      <p>{props.author}</p>
+    </li>
+  )
 }
 
 export default List
